3a885fb56b0fc8f94bc78124a54368cf7641c601
Candelaria Barcelo <barcelo.candelaria@gmail.com>
2022-07-03 23:53:58 -0300

    making icons change with light/dark mode

--- homeworks/candelaria-barcelo/04-css/index.js
+++ homeworks/candelaria-barcelo/04-css/index.js
@@ -1,14 +1,27 @@
 const toggle = document.getElementById("toggle");
-const theme = window.localStorage.getItem("theme");
+const githubIcon = document.getElementById("github-icon");
+const twitterIcon = document.getElementById("twitter-icon");
+const linkedinIcon = document.getElementById("linkedin-icon");
+const youtubeIcon = document.getElementById("youtube-icon");
 
-/* checks if the theme stored in localStorage is dark
-if yes apply the dark theme to the body */
-if (theme === "dark") document.body.classList.add("dark");
+let theme = "light";
 
 // event listener stops when the change theme button is clicked
 toggle.addEventListener("click", () => {
-   document.body.classList.toggle("dark");
-   if (theme === "dark") {
-     window.localStorage.setItem("theme", "light");
-   } else window.localStorage.setItem("theme", "dark");
+  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    toggle.src = "./images/icons/light-dark-mode.svg";
+    githubIcon.src = "./images/icons/github-icon.svg";
+    twitterIcon.src = "./images/icons/twitter-icon.svg";
+    linkedinIcon.src = "./images/icons/linkedin-icon.svg";
+    youtubeIcon.src = "./images/icons/youtube-icon.svg";
+    theme = "light";
+  } else if (theme === "light") {
+    toggle.src = "./images/icons/dark-mode.svg";
+    githubIcon.src = "./images/icons/dark-github-icon.svg";
+    twitterIcon.src = "./images/icons/dark-twitter-icon.svg";
+    linkedinIcon.src = "./images/icons/dark-linkedin-icon.svg";
+    youtubeIcon.src = "./images/icons/dark-youtube-icon.svg";
+    theme = "dark";
+  }
 });
