1db9bc66cb4cf5cd5b10d8557c3afde64b598883
Candelaria Barcelo <barcelo.candelaria@gmail.com>
2022-07-04 00:07:15 -0300

    making code clearer

--- homeworks/candelaria-barcelo/04-css/index.js
+++ homeworks/candelaria-barcelo/04-css/index.js
@@ -6,22 +6,29 @@ const youtubeIcon = document.getElementById("youtube-icon");
 
 let theme = "light";
 
-// event listener stops when the change theme button is clicked
+const setDarkIcons = () => {
+  toggle.src = "./images/icons/dark-mode.svg";
+  githubIcon.src = "./images/icons/dark-github-icon.svg";
+  twitterIcon.src = "./images/icons/dark-twitter-icon.svg";
+  linkedinIcon.src = "./images/icons/dark-linkedin-icon.svg";
+  youtubeIcon.src = "./images/icons/dark-youtube-icon.svg";
+};
+
+const setLightIcons = () => {
+  toggle.src = "./images/icons/light-dark-mode.svg";
+  githubIcon.src = "./images/icons/github-icon.svg";
+  twitterIcon.src = "./images/icons/twitter-icon.svg";
+  linkedinIcon.src = "./images/icons/linkedin-icon.svg";
+  youtubeIcon.src = "./images/icons/youtube-icon.svg";
+};
+
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   if (theme === "dark") {
-    toggle.src = "./images/icons/light-dark-mode.svg";
-    githubIcon.src = "./images/icons/github-icon.svg";
-    twitterIcon.src = "./images/icons/twitter-icon.svg";
-    linkedinIcon.src = "./images/icons/linkedin-icon.svg";
-    youtubeIcon.src = "./images/icons/youtube-icon.svg";
+    setLightIcons();
     theme = "light";
   } else if (theme === "light") {
-    toggle.src = "./images/icons/dark-mode.svg";
-    githubIcon.src = "./images/icons/dark-github-icon.svg";
-    twitterIcon.src = "./images/icons/dark-twitter-icon.svg";
-    linkedinIcon.src = "./images/icons/dark-linkedin-icon.svg";
-    youtubeIcon.src = "./images/icons/dark-youtube-icon.svg";
+    setDarkIcons();
     theme = "dark";
   }
 });
