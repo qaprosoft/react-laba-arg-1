486ef8f2c418f16d6d1315869c6e968fc3d823a6
Emiliano Acerbi <eacerbip@gmail.com>
2022-07-06 18:18:47 -0300

    changed name of variables to fit classes

--- homeworks/emiliano-acerbi/04-css/index.js
+++ homeworks/emiliano-acerbi/04-css/index.js
@@ -1,54 +1,52 @@
 // Elements
-const themeBtn = document.getElementById('themeBtn')
-const themeBtnLight = document.getElementById('themeBtn-light')
-const headerTitle = document.querySelector('.header__title')
-const breadCrumbs = document.querySelector('.main__breadcrumbs')
-const breadCrumb = document.querySelector('.main__breadcrumb')
-const btn = document.querySelector('.main__btn-secondary')
-const headerListLight = document.querySelector('.header__list-light')
-const headerListDark = document.querySelector('.header__list')
+const themeBtn = document.getElementById("themeBtn")
+const themeBtnLight = document.getElementById("themeBtn-light")
+const profileTitle = document.querySelector(".profile__title")
+const breadCrumbs = document.querySelector(".breadcrumbs")
+const breadCrumb = document.querySelector(".breadcrumbs__item")
+const btn = document.querySelector(".buttons__btn-secondary")
+const profileListLight = document.querySelector(".profile__list-light")
+const profileListDark = document.querySelector(".profile__list")
 
 // Variables
-const BACKGROUND_COLOR_LIGHT = '#e9ebec'
-const BACKGROUND_COLOR_DARK = '#0c151d'
-const TEXT_COLOR_DARK = '#a3abb2'
-const TEXT_COLOR_LIGHT = '#575757'
+const BACKGROUND_COLOR_LIGHT = "#e9ebec"
+const BACKGROUND_COLOR_DARK = "#0c151d"
+const TEXT_COLOR_DARK = "#a3abb2"
+const TEXT_COLOR_LIGHT = "#575757"
 let darkMode = true
 
 // Logic
-themeBtn.addEventListener('click', () => toggleTheme())
-themeBtnLight.addEventListener('click', () => toggleTheme())
+themeBtn.addEventListener("click", () => toggleTheme())
+themeBtnLight.addEventListener("click", () => toggleTheme())
 
 const toggleTheme = () => {
-	if (darkMode) {
-		document.body.style.backgroundColor = BACKGROUND_COLOR_LIGHT
-		document.body.style.color = TEXT_COLOR_LIGHT
-		themeBtn.style.display = 'none'
-		themeBtnLight.style.display = 'grid'
-		headerTitle.classList.add('light')
-		headerListLight.style.display = 'flex'
-		headerListDark.style.display = 'none'
-		btn.classList.add('light')
-		breadCrumbs.classList.add('light')
-		breadCrumb.classList.add('light')
+  if (darkMode) {
+    document.body.style.backgroundColor = BACKGROUND_COLOR_LIGHT
+    document.body.style.color = TEXT_COLOR_LIGHT
+    themeBtn.style.display = "none"
+    themeBtnLight.style.display = "grid"
+    profileTitle.classList.add("light")
+    profileListLight.style.display = "flex"
+    profileListDark.style.display = "none"
+    btn.classList.add("light")
+    breadCrumbs.classList.add("light")
+    breadCrumb.classList.add("light")
 
-		darkMode = !darkMode
-		return
-	}
+    darkMode = !darkMode
+    return
+  } else {
+    document.body.style.backgroundColor = BACKGROUND_COLOR_DARK
+    document.body.style.color = TEXT_COLOR_DARK
+    themeBtn.style.display = "grid"
+    themeBtnLight.style.display = "none"
+    profileTitle.classList.remove("light")
+    profileListLight.style.display = "none"
+    profileListDark.style.display = "flex"
+    btn.classList.remove("light")
+    breadCrumbs.classList.remove("light")
+    breadCrumb.classList.remove("light")
 
-	if (!darkMode) {
-		document.body.style.backgroundColor = BACKGROUND_COLOR_DARK
-		document.body.style.color = TEXT_COLOR_DARK
-		themeBtn.style.display = 'grid'
-		themeBtnLight.style.display = 'none'
-		headerTitle.classList.remove('light')
-		headerListLight.style.display = 'none'
-		headerListDark.style.display = 'flex'
-		btn.classList.remove('light')
-		breadCrumbs.classList.remove('light')
-		breadCrumb.classList.remove('light')
-
-		darkMode = !darkMode
-		return
-	}
+    darkMode = !darkMode
+    return
+  }
 }
