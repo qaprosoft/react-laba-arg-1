6e106dd3cbabff5c279777a03fd149b9635c9491
Federico Benitez <fedebenitez1696@gmail.com>
2022-07-12 18:41:31 -0300

    feat: button for scroll to top

--- homeworks/federico-benitez/05-css-advanced/js/scroll.js
+++ homeworks/federico-benitez/05-css-advanced/js/scroll.js
@@ -1,11 +1,15 @@
 function scrollDetect() {
   window.onscroll = function () {
-    let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
+    const currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
+    const button = document.getElementById('scrollButton');
 
-    if (currentScroll > 0) {
-      document.getElementById('scrollLoc').addEventListener('click', () => {
+    if (currentScroll > 30) {
+      button.style.display = 'block';
+      button.addEventListener('click', () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
       });
+    } else {
+      button.style.display = 'none';
     }
   };
 }
