c4425925a11f692f80a51fb937fb86fafb3d8a84
Mykhailo Melnyk <mmelnyk@solvd.com>
2022-07-15 17:27:25 +0300

    Add 6th lecture

---
+++ lectures/06-js-basics-1/practice/basics.js
@@ -0,0 +1,9 @@
+// katas 1 https://www.codewars.com/kata/opposite-number
+function oppositeNumber(number) {
+    return number * -1;
+}
+
+// katas 2 https://www.codewars.com/kata/opposite-number
+function mathematicOperation(sign, number1, number2) {
+    return number1 + sign + number2
+}
