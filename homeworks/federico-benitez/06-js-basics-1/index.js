a5bac0fa19a70eb0afe8175e08586b362125f6c8
Federico Benitez <fedebenitez1696@gmail.com>
2022-07-20 20:05:18 -0300

    exercise 13

--- homeworks/federico-benitez/06-js-basics-1/index.js
+++ homeworks/federico-benitez/06-js-basics-1/index.js
@@ -244,3 +244,19 @@ function FilterString(value) {
       .join(''),
   );
 }
+
+/**
+ * Exercise 13 - n-th Fibonacci
+ * https://www.codewars.com/kata/n-th-fibonacci
+ */
+
+function nthFibo(n) {
+  // Return the n-th number in the Fibonacci Sequence
+  const fib = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib[n - 1];
+}
