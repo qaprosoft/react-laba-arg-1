81d0a1505ad3e4a3b7ef87a1a05fd81800110bfc
Federico Benitez <fedebenitez1696@gmail.com>
2022-07-19 21:09:08 -0300

    exercise 2 added

--- homeworks/federico-benitez/06-js-basics-1/index.js
+++ homeworks/federico-benitez/06-js-basics-1/index.js
@@ -6,3 +6,23 @@
 function opposite(number) {
   return number * -1;
 }
+
+/**
+ * Exercise 2 - Basic Mathematical Operations
+ * http://www.codewars.com/kata/basic-mathematical-operations
+ */
+
+function basicOp(operation, a, b) {
+  switch (operation) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    default:
+      throw new Error(`Operator ${operation} not implemented `);
+  }
+}
