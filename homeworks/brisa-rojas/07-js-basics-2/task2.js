4c5848200cd7feb55ab8097d76667fc8d550ad23
Brisa Rojas <brojas@itba.edu.ar>
2022-07-22 12:10:40 -0300

    adds kata 3

--- homeworks/brisa-rojas/07-js-basics-2/task2.js
+++ homeworks/brisa-rojas/07-js-basics-2/task2.js
@@ -10,6 +10,10 @@ function positiveSum(arr) {
 
 // 2 - https://www.codewars.com/kata/5a3e1319b6486ac96f000049
 // 3 - https://www.codewars.com/kata/5aba780a6a176b029800041c
+function maxMultiple(divisor, bound){
+    return divisor * Math.floor(bound/divisor);
+}
+
 // 4 - https://www.codewars.com/kata/514a6336889283a3d2000001
 function getEvenNumbers(numbersArray){
     return numbersArray.filter(number => !(number % 2));
@@ -93,6 +97,8 @@ function findUniq(arr) {
 }
 
 // 10 - https://www.codewars.com/kata/581e014b55f2c52bb00000f8
+
+
 // 11 - https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
 function sortArray(array) {
     let odds = [];
