74e7f6d3e3fc61af28739967dcef66f4ae90bb46
Brisa Rojas <brojas@itba.edu.ar>
2022-07-22 13:34:20 -0300

    adds optional kata #1

--- homeworks/brisa-rojas/07-js-basics-2/task2.js
+++ homeworks/brisa-rojas/07-js-basics-2/task2.js
@@ -160,16 +160,58 @@ function moveZeros(arr) {
     
     //delete zeros in the middle
     while(arr.includes(0)){
-      arr.splice(arr.indexOf(0),1);
-      zeros += 1;
+        arr.splice(arr.indexOf(0),1);
+        zeros += 1;
     }
      
     console.log("out");
     //add zeros at the end
     while(zeros){
-      arr.push(0);
-      zeros -= 1;
+        arr.push(0);
+        zeros -= 1;
     }
   
     return arr;
-  }
+}
+
+// https://www.codewars.com/kata/515bb423de843ea99400000a
+// TODO: complete this object/class
+
+// The constructor takes in an array of items and a integer indicating how many
+// items fit within a single page
+function PaginationHelper(collection, itemsPerPage){
+    this.items = collection;
+    this.itemsPerPage = itemsPerPage;
+}
+  
+    // returns the number of items within the entire collection
+    PaginationHelper.prototype.itemCount = function() {
+        return this.items.length;
+    }
+  
+    // returns the number of pages
+    PaginationHelper.prototype.pageCount = function() {
+        return Math.ceil(this.items.length/this.itemsPerPage) ;
+    }
+  
+    // returns the number of items on the current page. page_index is zero based.
+    // this method should return -1 for pageIndex values that are out of range
+    PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+        if (pageIndex > this.pageCount || pageIndex < 0){
+            return -1;
+        }
+        else{
+            return (this.items).slice(pageIndex * this.itemsPerPage, this.itemCount()).splice(0, this.itemsPerPage).length;
+            // slice to "ignore" everything in previous pages, SPLICE to get AT MOST itemCounts elements, then get length
+        }
+    }
+  
+    // determines what page an item is on. Zero based indexes
+    // this method should return -1 for itemIndex values that are out of range
+    PaginationHelper.prototype.pageIndex = function(itemIndex) {
+        if (itemIndex < 0 || itemIndex > this.items.length || this.items.length == 0){ return (-1);}
+        return Math.floor( itemIndex / this.itemsPerPage);
+    }
+
+    //this kata passes all test but one in "attempt" but as it does not show tests, I can't figure out why it fails and fix it
+    // pageItemCount is returning incorrect value
