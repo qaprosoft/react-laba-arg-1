6c8349ae75310f2f39de7df5a149da7abcf34222
Emiliano Acerbi <eacerbip@gmail.com>
2022-07-22 15:41:16 -0300

    kata 3 4 and 5

--- homeworks/emiliano-acerbi/07-js-basics-2/index.js
+++ homeworks/emiliano-acerbi/07-js-basics-2/index.js
@@ -26,3 +26,40 @@ function pairs(arr) {
     return acc;
   }, 0);
 }
+
+// 3. https://www.codewars.com/kata/5aba780a6a176b029800041c
+
+function maxMultiple(divisor, bound) {
+  let count = bound;
+  while (count > divisor) {
+    if (count % divisor === 0) {
+      return count;
+    }
+    count--;
+  }
+}
+
+// 4. https://www.codewars.com/kata/514a6336889283a3d2000001
+
+function getEvenNumbers(numbersArray) {
+  return numbersArray.filter((el) => el % 2 === 0);
+}
+
+// 5. https://www.codewars.com/kata/5a090c4e697598d0b9000004
+
+function solve(arr) {
+  let res = [];
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (i % 2 === 0) {
+      let max = Math.max(...arr);
+      arr.splice(arr.indexOf(max), 1);
+      res.push(max);
+    } else {
+      let min = Math.min(...arr);
+      arr.splice(arr.indexOf(min), 1);
+      res.push(min);
+    }
+  }
+  return res;
+}
