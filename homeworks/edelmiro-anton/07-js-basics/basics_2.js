b0d3df5988a644f9b7804825b1336f68a58d32df
Edelmiro Anton <anton.edelmiro@gmail.com>
2022-07-24 20:50:12 -0300

    Add Kata #2

--- homeworks/edelmiro-anton/07-js-basics/basics_2.js
+++ homeworks/edelmiro-anton/07-js-basics/basics_2.js
@@ -21,3 +21,22 @@ function positiveSum(arr) {
 
   return sum;
 }
+
+// Task 2 - https://www.codewars.com/kata/5a3e1319b6486ac96f000049
+function pairs(ar) {
+  let pairs = Math.floor(ar.length / 2);
+  let arr2 = [];
+  let countPairs = 0;
+
+  for (i = 0; i < pairs; i++) {
+    let splice = ar.splice(0, 2);
+    arr2.push(splice);
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i][0] - arr2[i][1] == -1 || arr2[i][0] - arr2[i][1] == 1) {
+      countPairs++;
+    }
+  }
+  return countPairs;
+}
