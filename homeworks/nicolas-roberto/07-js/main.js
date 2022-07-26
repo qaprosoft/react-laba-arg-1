2c14748923fbb31d045c164ed76dc714020e0fb1
Nicolás <nicolaseugroberto@gmail.com>
2022-07-26 20:07:21 -0300

    First cata solved, with commentary

---
+++ homeworks/nicolas-roberto/07-js/main.js
@@ -0,0 +1,28 @@
+//Codewars tasks
+// kata 01 https://www.codewars.com/kata/5715eaedb436cf5606000381
+
+function positiveSum(array) {
+  let sumArray = 0;
+  array.forEach((element) => {
+    if (element > 0) {
+      sumArray += element;
+    }
+  });
+  return sumArray;
+}
+// kata 02 https://www.codewars.com/kata/5a3e1319b6486ac96f000049
+// kata 03 https://www.codewars.com/kata/5aba780a6a176b029800041c
+// kata 04 https://www.codewars.com/kata/514a6336889283a3d2000001
+// kata 05 https://www.codewars.com/kata/5a090c4e697598d0b9000004
+// kata 06 https://www.codewars.com/kata/566044325f8fddc1c000002c
+// kata 07 https://www.codewars.com/kata/545a4c5a61aa4c6916000755
+// kata 08 https://www.codewars.com/kata/578553c3a1b8d5c40300037c
+// kata 09 https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+// kata 10 https://www.codewars.com/kata/581e014b55f2c52bb00000f8
+// kata 11 https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+
+//Optional (advanced)
+// kata 01 https://www.codewars.com/kata/515bb423de843ea99400000a
+// kata 02 https://www.codewars.com/kata/52597aa56021e91c93000cb0
+// kata 03 https://www.codewars.com/kata/585d8c8a28bc7403ea0000c3
+// kata 04 https://www.codewars.com/kata/5296bc77afba8baa690002d7
