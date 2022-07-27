a1f9be24584814e494808b3ccaa32eb0b00c00f3
Edelmiro Anton <anton.edelmiro@gmail.com>
2022-07-27 20:57:24 -0300

    Fixed Kata 18

--- homeworks/edelmiro-anton/06-JS-basics/basics_1.js
+++ homeworks/edelmiro-anton/06-JS-basics/basics_1.js
@@ -248,23 +248,23 @@ function duplicateEncode(word) {
 // Task 18 - https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/train/javascript
 // Mexican Wave
 
-let str = 'hola';
-let strLower = str.toLowerCase(); // To convert all characters in lower case in case there are some upper
-strLower.trim();
-let arr = [];
+function wave(str) {
+  let strLower = str.toLowerCase(); // To convert all characters in lower case in case there are some upper
+  strLower.trim();
+  let arr = [];
 
-for (let i = 0; i < strLower.length; i++) {
-  let substring = strLower.substring(i, 0); // empty string when substring(0,0)
-  let char = strLower[i];
-  let slice = strLower.slice(i + 1);
+  for (let i = 0; i < strLower.length; i++) {
+    let substring = strLower.substring(i, 0); // empty string when substring(0,0)
+    let char = strLower[i];
+    let slice = strLower.slice(i + 1);
 
-  if (!char.includes(' ')) {
-    let newStr = substring + char.toUpperCase() + slice;
-    arr.push(newStr);
+    if (!char.includes(' ')) {
+      let newStr = substring + char.toUpperCase() + slice;
+      arr.push(newStr);
+    }
   }
+  return arr;
 }
-
-console.log(arr);
 
 // Task 20 - https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
 
