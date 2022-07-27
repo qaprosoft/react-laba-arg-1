6b07048de133492d3715087a42b69fe427b90e22
Nicolás <nicolaseugroberto@gmail.com>
2022-07-26 21:53:16 -0300

    Second cata solved, with commentary

--- homeworks/nicolas-roberto/07-js/main.js
+++ homeworks/nicolas-roberto/07-js/main.js
@@ -2,15 +2,42 @@
 // kata 01 https://www.codewars.com/kata/5715eaedb436cf5606000381
 
 function positiveSum(array) {
+  //We define a variable with an initial value of 0
   let sumArray = 0;
+  //With forEach, we will iterate each element of the given array.
   array.forEach((element) => {
+    //If the current element of the array is positive (>0)
     if (element > 0) {
+      //Adds up the current element value to sumArray
       sumArray += element;
+      //If this condition (element > 0) is false, it won't do anything
+      //And it will continue with the loop until it finishes and returns sumArray
     }
   });
+  //If the length of the array is 0 or 1 and the value is 0
+  //It will automatically return sumArray equals to 0
   return sumArray;
 }
+
 // kata 02 https://www.codewars.com/kata/5a3e1319b6486ac96f000049
+
+function pairs(array) {
+  //We create a pair count
+  let pairCount = 0;
+  //For each iteration we sum +2 to index (because iterations must be in pairs)
+  for (let index = 0; index < array.length; index = index++) {
+    //We create a new array which contains the first two elements of the original array (splice)
+    let newArray = array.splice(0, 2);
+    //The conditions: if the new array is length 2 (so if the last element remaining is one, it wont apply)
+    //AND one of the elements +1 is equal to the other element or vice versa (it means they're consecutives)
+    if ((newArray.length === 2 && newArray[1] + 1 === newArray[0]) || newArray[1] - 1 === newArray[0]) {
+      //We add +1 to out pair count.
+      pairCount++;
+    }
+  } //And return pairCount once the iterations of the original array are finished
+  return pairCount;
+}
+
 // kata 03 https://www.codewars.com/kata/5aba780a6a176b029800041c
 // kata 04 https://www.codewars.com/kata/514a6336889283a3d2000001
 // kata 05 https://www.codewars.com/kata/5a090c4e697598d0b9000004
