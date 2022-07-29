224c34c29abdbecc8ac452be4bb1e16092b45ac4
Brisa Rojas <brojas@itba.edu.ar>
2022-07-29 18:42:32 -0300

    Feat: deep clone function

--- homeworks/brisa-rojas/08-js-advanced-1/task3.js
+++ homeworks/brisa-rojas/08-js-advanced-1/task3.js
@@ -1,5 +1,6 @@
-// 1 - PLUCK
-// Create a function to access the properties of an object.
+// VARIABLE DECLARATIONS
+
+// Object for pluck and clone functions
 const user = {
   username: 'testuser1',
   preferences: {
@@ -9,9 +10,18 @@ const user = {
     },
   },
 };
+// for pluck function
 const randomValue = Math.random();
 const nullValue = null;
 
+// for clone function
+let clonedUser = clone(user);
+clonedUser.preferences.sound.maxValue = 70; // change the value of the sound.maxValue property of the cloned object
+
+
+
+// 1 - PLUCK
+// Create a function to access the properties of an object.
 function pluck(obj, key) {
   let searchedValue = nullValue;
   let keys = key.split('.');
@@ -37,3 +47,13 @@ console.log(pluck(nullValue, 'unknown.key')); // null
 //Other tests
 console.log(pluck(user, 'username'));
 console.log(pluck(user, 'preferences.sound.maxValue'));
+
+
+// 2 - DEEP CLONE
+//Create custom deep clone function.
+function clone(obj){
+    return JSON.parse(JSON.stringify(obj)); 
+    // user obj is small enough to be cloned with JSON.parse/JSON.stringify approach
+}
+
+console.log(user.preferences.sound.maxValue === clonedUser.preferences.sound.maxValue); // false
