b4aadf1b7392d7d64324228d07425734a5a15610
German O. Macchia <germanmacchia@gmail.com>
2022-07-29 02:07:47 -0300

    Task 6 - "this" is an other problem

--- homeworks/german-macchia/08-js-advanced-1/advanced-1.js
+++ homeworks/german-macchia/08-js-advanced-1/advanced-1.js
@@ -101,3 +101,19 @@ function objConcat(objectsArray) {
 }
 
 //6. "this" is an other problem https://www.codewars.com/kata/547f1a8d4a437abdf800055c
+function NamedOne(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+
+  Object.defineProperty(this, "fullName", {
+    get: function () {
+      return this.firstName + " " + this.lastName;
+    },
+    set(_fullName) {
+      _fullName = _fullName.split(" ");
+      if (_fullName.length === 2) {
+        [this.firstName, this.lastName] = _fullName;
+      }
+    },
+  });
+}
