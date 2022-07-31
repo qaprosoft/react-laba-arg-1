789ad50d2b377db649b370ad82fdd052ba8f8ead
ilya-ischenko <iischenko@solvd.com>
2022-07-31 10:59:54 +0300

    added 1st part practire for lecture 11

---
+++ lectures/11-js-dom/practice-1/script/1-navigation.js
@@ -0,0 +1,33 @@
+// console.log(document.documentElement); // html
+// console.log(document.body); // body
+// console.log(document.head); // head
+
+// const bodyChildrenList = document.body.children;
+// const bodyChildList = document.body.childNodes;
+// console.log('bodyChildrenList', bodyChildrenList);
+// console.log('bodyChildList', bodyChildList);
+
+// const firstBodyChild = document.body.firstChild;
+// const firstBodyChildren = document.body.firstElementChild;
+// console.log('firstBodyChild', firstBodyChild);
+// console.log('firstBodyChildren', firstBodyChildren);
+
+// const firstBodyChildren = document.body.firstElementChild;
+// console.log(firstBodyChildren);
+// console.log(firstBodyChildren.nextElementSibling);
+
+/**
+ * SEARCHING
+ */
+// const byTagName = document.querySelector('a');
+// const byClassName = document.querySelector('.profile');
+// const byId = document.querySelector('#top');
+// const byAttribute = document.querySelectorAll('[href]');
+// console.log('byTagName', byTagName);
+// console.log('byClassName', byClassName);
+// console.log('byId', byId);
+// console.log(`byAttribute`, byAttribute);
+
+// const profileFactsItem = document.querySelector('.profile-facts__item');
+// const closestSection = profileFactsItem.closest('section');
+// console.log(`closestSection`, closestSection);
---
+++ lectures/11-js-dom/practice-1/script/7-events.js
@@ -0,0 +1,22 @@
+// const testBtn = document.querySelector('#test-btn');
+
+// const clickHandler = (e) => {
+//   console.log(e.target);
+// };
+
+// testBtn.addEventListener('click', clickHandler);
+
+// testBtn.addEventListener('click', clickHandler);
+// testBtn.removeEventListener('click', clickHandler);
+
+// const btn = document.querySelector('#button');
+// const divOuter = document.querySelector('.div-outer');
+// const divInner = document.querySelector('.div-inner');
+// const p = document.querySelector('p');
+
+// divOuter.addEventListener('click', () => console.log('div outer CLICK'));
+// divInner.addEventListener('click', () => console.log('div inner CLICK'));
+// p.addEventListener('click', () => console.log('p CLICK'));
+// btn.addEventListener('click', () => console.log('BUTTON CLICK'));
+
+// preventDefault();
